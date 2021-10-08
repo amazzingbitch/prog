66e94083b6f7bc40781ab59005e1eff054537dee
amazzingbitch <71969370+amazzingbitch@users.noreply.github.com>
2021-10-09 03:04:43 +0700

    friend operators added

---
+++ cmake-build-debug/CMakeFiles/prog2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog2.dir/Data.cpp.obj"
+  "CMakeFiles/prog2.dir/main.cpp.obj"
+  "CMakeFiles/prog2.dir/test.cpp.obj"
+  "libprog2.dll.a"
+  "prog2.exe"
+  "prog2.exe.manifest"
+  "prog2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_RUNNER
+
+#include "catch.hpp"
+
+using namespace std;
+
+int main() {
+    Catch::Session().run();
+    return 0;
+}
+
