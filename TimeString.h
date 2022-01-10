ac0bc78e532a270b61120b2672b8544103a1ce61
amazzingbitch <71969370+amazzingbitch@users.noreply.github.com>
2022-01-11 02:06:56 +0700

    add comments

--- TimeString.h
+++ TimeString.h
@@ -7,24 +7,46 @@
 
 #include "Date.h"
 
-class TimeString : public Date {
+class Event : public Date {
 public:
-    TimeString();
-    TimeString(int day, int month, int year, int hour, int minute, int second);
-    TimeString(TimeString &a);
-    TimeString(const TimeString &a);
-    ~TimeString();
-    void ToStringTime();
-    char* GetTime();
+    // конструктор по умолчанию
+    Event();
+    // конструктор с параметрами
+    Event(int day, int month, int year, int hour, int minute, int second);
+    // конструктор с параметрами
+    Event(int day, int month, int year, int hour, int minute, int second, const char* name);
+    // конструктор копирования
+    Event(Event &a);
+    // конструктор копирования с константным объектом
+    Event(const Event &a);
+    // деструктор
+    ~Event();
+    // метод заполнения строковых полей, для конструкторов
+    void ToStringEvent();
+    // метод устанавливающий значение названия мероприятия
+    void SetEvent(const char* name);
+    // метод получения строкового представления даты и времени
+    char* GetStrDate();
+    // метод получения строкого представления даты, времени и мероприятия
+    char* GetStr() override;
+    // метод заполнения строковых полей
     void ToString() override;
-    friend TimeString operator+(const TimeString &d1, const TimeString &d2);
-    friend TimeString operator-(const TimeString &d1, const TimeString &d2);
-    friend TimeString operator+(const TimeString &d1, int hour);
-    friend TimeString operator-(const TimeString &d1, int hour);
-    TimeString &operator=(const TimeString &right);
+    // оператор сложения дат
+    friend Event operator+(const Event &d1, const Event &d2);
+    // оператор вычитания дат
+    friend Event operator-(const Event &d1, const Event &d2);
+    // оператор сложения числа дней, часов
+    friend Event operator+(const Event &d1, int hour);
+    // оператор вычитания числа дней, часов
+    friend Event operator-(const Event &d1, int hour);
+    // оператор присваивания
+    Event &operator=(const Event &right);
 
 protected:
-    char* time;
+    // поле строкового представления даты, времени и названия мероприятия
+    char* event;
+    // поле для названия мероприятия
+    char* eventName;
 };
 
 #endif //DATE_CPP_TIMESTRING_H
